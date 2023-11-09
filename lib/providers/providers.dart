1d41b530c07a358a7f1a44e969d3eee4eb2d40cf
Azkina1123 <aoshofrina@gmail.com>
2023-11-09 15:50:56 +0800

    Update providers.dart

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 
 import 'package:post_in/models/models.dart';
 
-part 'post_provider.dart';
-part 'page_provider.dart';
-part 'komentar_provider.dart';
-part 'user_provider.dart';
-part 'auth_provider.dart';
-part 'like_provider.dart';
-part 'following_provider.dart';
+part 'post_data.dart';
+part 'page_data.dart';
+part 'komentar_data.dart';
+part 'user_data.dart';
+part 'auth_data.dart';
+part 'like_data.dart';
+part 'following_data.dart';
