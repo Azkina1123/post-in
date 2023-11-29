356cdf2fac58912c33c094b48c1a482421cb0cf8
Chintialiuw <chintialiu186@gmail.com>
2023-11-30 03:16:20 +0800

    Update: baru berhasil nampilkan postingan

--- lib/ui/pages/cari_page.dart
+++ lib/ui/pages/cari_page.dart
@@ -1,13 +1,14 @@
 part of "pages.dart";
 
 class CariPage extends StatefulWidget {
-  const CariPage({super.key});
+  const CariPage({Key? key}) : super(key: key);
 
   @override
-  State<CariPage> createState() => _CariPageState();
+  _CariPageState createState() => _CariPageState();
 }
 
 class _CariPageState extends State<CariPage> {
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +35,15 @@ class _CariPageState extends State<CariPage> {
             TabBar(
               tabs: const [
                 Tab(text: "Akun"),
-                Tab(text: "Postingan")
+                Tab(text: "Postingan"),
               ],
               labelStyle: TextStyle(
                 color: Theme.of(context).colorScheme.primary,
                 fontSize: Theme.of(context).textTheme.titleMedium!.fontSize,
                 fontWeight: FontWeight.bold,
               ),
-              labelPadding: EdgeInsets.only(left: 10, right: 10),
-              indicatorColor: Theme.of(context).colorScheme.primary, 
+              labelPadding: const EdgeInsets.only(left: 10, right: 10),
+              indicatorColor: Theme.of(context).colorScheme.primary,
               indicatorWeight: 2,
               dividerColor: Theme.of(context)
                   .colorScheme
@@ -52,16 +53,57 @@ class _CariPageState extends State<CariPage> {
             Expanded(
               child: TabBarView(
                 children: [
-                  Center(
-                    child: Text(
-                      "Nanti diisi sama history akun yang dicari"
-                    )
-                  ),
-                  Center(
-                    child: Text(
-                      "Nanti diisi sama history postingan yang dicari"
-                    )
-                  ),
+                  // if(_index == 0)
+                  //   Center(
+                  //     child: Text("Testing"),
+                  //   )
+                  if (_index == 0)
+                    StreamBuilder<QuerySnapshot>(
+                      stream: _getSnapshot(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return Container(
+                            width: width(context),
+                            alignment: Alignment.center,
+                            child: const CircularProgressIndicator(),
+                          );
+                        } else if (snapshot.hasData) {
+                          final posts = snapshot.data!.docs;
+
+                          return Column(
+                            children: [
+                              for (int i = 0; i < posts.length; i++)
+                                Column(
+                                  children: [
+                                    PostWidget(
+                                      post: Post.fromJson(posts[i].data()
+                                          as Map<String, dynamic>),
+                                    ),
+
+                                    // Kasih pembatas antar post --------------------------------------
+                                    if (i != posts.length - 1)
+                                      Divider(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .tertiary
+                                            .withOpacity(0.5),
+                                        indent: 10,
+                                        endIndent: 10,
+                                      )
+                                    // Di post terakhir tidak perlu pembatas -------------------------
+                                    else
+                                      const SizedBox(
+                                        height: 20,
+                                      )
+                                  ],
+                                )
+                            ],
+                          );
+                        }
+
+                        return const Text("Belum ada post yang ditambahkan.");
+                      },
+                    ),
                 ],
               ),
             ),
@@ -69,5 +111,24 @@ class _CariPageState extends State<CariPage> {
         ),
       ),
     );
+  }
+
+  Stream<QuerySnapshot<Object?>> _getSnapshot() {
+    switch (_index) {
+      case 0:
+        return Provider.of<PostData>(context)
+            .postsCollection
+            .orderBy("tglDibuat", descending: true)
+            .snapshots();
+      case 1:
+        return Provider.of<PostData>(context)
+            .postsCollection
+            .orderBy("totalLike", descending: true)
+            .orderBy("totalKomentar", descending: true)
+            .orderBy("tglDibuat", descending: true)
+            .snapshots();
+      default:
+        throw Exception("Indeks $_index tidak valid.");
+    }
   }
 }
