13c3ad5b3920986ca3614080a8f9501b0f38ae8c
Azkina1123 <aoshofrina@gmail.com>
2023-11-30 16:56:55 +0800

    fix: debugging

--- lib/ui/pages/debugPage.dart
+++ lib/ui/pages/debugPage.dart
@@ -1,49 +1,50 @@
 part of "pages.dart";
 
-class DebugPage extends StatelessWidget {
+class DebugPage extends StatefulWidget {
   const DebugPage({super.key});
 
   @override
+  State<DebugPage> createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
+  TextEditingController _con = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    // Provider.of<UserData>(context, listen: false).add(
-    //     User(
-    //       id: "",
-    //       tglDibuat: DateTime.now(),
-    //       username: "bebek",
-    //       namaLengkap: "Bebek Geprek",
-    //       email: "bebeik.bakar@gmail.com",
-    //       password: "nyamnyam",
-    //       gender: "Laki-laki",
-    //       noTelp: "123445667",
-    //       sampul: "",
-    //       foto:
-    //           "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/06/14024013/Tak-Hanya-Lezat-Ini-X-Manfaat-Bayam-bagi-Kesehatan-Tubuh-01.jpg",
-    //     ),
-    //   );
     return Scaffold(
-      body: Container(
-        // color: Colors.amber,
-        alignment: Alignment.center,
-        child: FutureBuilder<List<String>>(
-          future: Provider.of<UserData>(context, listen: false)
-              .getUserFollowerIds(FirebaseAuth.instance.currentUser!.uid),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              List<String> ids = snapshot.data!;
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text("DEBUG PAGE"),
-                  Text(ids.length.toString()),
-                  for (int i = 0; i < ids.length; i++) Text(ids[i]),
-                ],
-              );
-            }
-            return Text("Gagal ambil data");
+      appBar: AppBar(
+        title: TextField(
+          controller: _con,
+          onSubmitted: (value) {
+            setState(() {
+              _con.text = value;
+            });
           },
         ),
       ),
+      body: Container(
+          // color: Colors.amber,
+          alignment: Alignment.center,
+          child: StreamBuilder(
+            stream: Provider.of<PostData>(context, listen: false)
+                .postsCollection
+                .where("konten", isGreaterThanOrEqualTo: _con.text)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final posts = snapshot.data!.docs;
+                return ListView(
+                  children: [
+                    for (int i = 0; i < posts.length; i++)
+                      Text(
+                          Post.fromJson(posts[i].data() as Map<String, dynamic>)
+                              .konten)
+                  ],
+                );
+              }
+              return Text("Loading...");
+            },
+          )),
     );
   }
 }
