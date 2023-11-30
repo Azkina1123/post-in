d7ae3391c2fad3ba8b9314bd4db0816a6c84765f
Chintialiuw <chintialiu186@gmail.com>
2023-11-30 19:21:57 +0800

    Update: sisa postingan

--- lib/ui/pages/cari_page.dart
+++ lib/ui/pages/cari_page.dart
@@ -9,17 +9,28 @@ class CariPage extends StatefulWidget {
 
 class _CariPageState extends State<CariPage> {
   int _index = 0;
+  bool _search = false;
+  TextEditingController _ctrlCari = TextEditingController();
+  List<Post> PostCari = [];
+  List<UserAcc> UserCari = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        leading: null,
         scrolledUnderElevation: 0,
         title: SizedBox(
           height: 40,
           child: TextField(
+            controller: _ctrlCari,
+            onSubmitted: (query) {
+              setState(() {
+                _ctrlCari.text = query;
+                _search = true;
+                //getPostingan();
+              });
+            },
             decoration: InputDecoration(
               prefixIcon: Icon(Icons.search),
               hintText: 'Cari',
@@ -33,6 +44,11 @@ class _CariPageState extends State<CariPage> {
         child: Column(
           children: [
             TabBar(
+              onTap: (index) {
+                setState(() {
+                  _index = index;
+                });
+              },
               tabs: const [
                 Tab(text: "Akun"),
                 Tab(text: "Postingan"),
@@ -45,90 +61,110 @@ class _CariPageState extends State<CariPage> {
               labelPadding: const EdgeInsets.only(left: 10, right: 10),
               indicatorColor: Theme.of(context).colorScheme.primary,
               indicatorWeight: 2,
-              dividerColor: Theme.of(context)
-                  .colorScheme
-                  .tertiary
-                  .withOpacity(0.5),
+              dividerColor:
+                  Theme.of(context).colorScheme.tertiary.withOpacity(0.5),
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  // if(_index == 0)
-                  //   Center(
-                  //     child: Text("Testing"),
-                  //   )
-                  if (_index == 0)
-                    StreamBuilder<QuerySnapshot>(
-                      stream: _getSnapshot(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return Container(
-                            width: width(context),
-                            alignment: Alignment.center,
-                            child: const CircularProgressIndicator(),
-                          );
-                        } else if (snapshot.hasData) {
-                          final posts = snapshot.data!.docs;
-
-                          return Column(
-                            children: [
-                              for (int i = 0; i < posts.length; i++)
-                                Column(
-                                  children: [
-                                    PostWidget(
-                                      post: Post.fromJson(posts[i].data()
-                                          as Map<String, dynamic>),
-                                    ),
-
-                                    // Kasih pembatas antar post --------------------------------------
-                                    if (i != posts.length - 1)
-                                      Divider(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .tertiary
-                                            .withOpacity(0.5),
-                                        indent: 10,
-                                        endIndent: 10,
-                                      )
-                                    // Di post terakhir tidak perlu pembatas -------------------------
-                                    else
-                                      const SizedBox(
-                                        height: 20,
-                                      )
-                                  ],
-                                )
-                            ],
-                          );
-                        }
-
-                        return const Text("Belum ada post yang ditambahkan.");
-                      },
-                    ),
-                ],
-              ),
-            ),
+            Text(_ctrlCari.text),
+            _ctrlCari.text.isEmpty && !_search
+                ? Text(_index == 0
+                    ? "Belum ada akun yang dicari."
+                    : "Belum ada postingan yang dicari.")
+                : (_index == 0 ? showAkun() : showPostingan())
           ],
         ),
       ),
     );
   }
 
-  Stream<QuerySnapshot<Object?>> _getSnapshot() {
-    switch (_index) {
-      case 0:
-        return Provider.of<PostData>(context)
+  void getPostingan() async {
+    QuerySnapshot postingan = await Provider.of<PostData>(context, listen: false)
+        .postsCollection
+        .where("konten", isEqualTo: _ctrlCari.text)
+        .orderBy("tglDibuat", descending: true)
+        .get();
+    final posts = postingan.docs;
+    posts.forEach((post) {
+      PostCari.add(Post.fromJson(post.data() as Map<String, dynamic>));
+    });
+  }
+
+  Widget showPostingan() {
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Provider.of<PostData>(context, listen: false)
             .postsCollection
+            .where("konten", isEqualTo: _ctrlCari.text)
+            // .orderBy("tglDibuat", descending: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              width: width(context),
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            final posts = snapshot.data!.docs;
+            // posts.forEach((post) {
+            //   PostCari.add(
+            //     Post.fromJson(post.data() as Map<String, dynamic>),
+            //   );
+            // });
+            Column(
+        children: [
+          Text(posts[0].get("konten")),
+          for (int i = 0; i < posts.length; i++)
+            Column(
+              children: [
+                PostWidget(post: Post.fromJson(posts[i].data() as Map<String, dynamic>)),
+                // Kasih pembatas antar post --------------------------------------
+                if (i != posts.length - 1)
+                  Divider(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .tertiary
+                        .withOpacity(0.5),
+                    indent: 10,
+                    endIndent: 10,
+                  )
+                // Di post terakhir tidak perlu pembatas -------------------------
+                else
+                  const SizedBox(
+                    height: 20,
+                  )
+              ],
+            )
+        ],
+      );
+          }
+          return const Text("Belum ada post yang ditambahkan.");
+        },
+      ),
+    );
+  }
+
+  Widget showAkun() {
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Provider.of<UserData>(context, listen: false)
+            .usersCollection
+            .where("username", isEqualTo: _ctrlCari.text)
             .orderBy("tglDibuat", descending: true)
-            .snapshots();
-      case 1:
-        return Provider.of<PostData>(context)
-            .postsCollection
-            .orderBy("totalLike", descending: true)
-            .orderBy("totalKomentar", descending: true)
-            .orderBy("tglDibuat", descending: true)
-            .snapshots();
-      default:
-        throw Exception("Indeks $_index tidak valid.");
-    }
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              width: width(context),
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            final users = snapshot.data!.docs;
+            return Text(users[0].get("username"));
+          }
+          return const Text("Belum ada post yang ditambahkan.");
+        },
+      ),
+    );
   }
 }
