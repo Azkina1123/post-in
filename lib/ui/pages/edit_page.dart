f1ec01252737578f903b399b3fdf2c762ba569e3
Chintialiuw <chintialiu186@gmail.com>
2023-12-03 02:17:06 +0800

    Update: edit profil (belum bisa foto)

--- lib/ui/pages/edit_page.dart
+++ lib/ui/pages/edit_page.dart
@@ -15,15 +15,42 @@ class _EditPageState extends State<EditPage> {
 
   String? _profileImagePath;
   String? _coverImagePath;
-  String? url;
-  int randomNumber = Random().nextInt(99999999 - 10000000 + 1) + 10000000;
-
-  final FocusNode _focus = FocusNode();
+  String? urlProfile;
+  String? urlSampul;
 
   TextEditingController _ctrlNama = TextEditingController();
   TextEditingController _ctrlEmail = TextEditingController();
   TextEditingController _ctrlUsername = TextEditingController();
   TextEditingController _ctrlNomor = TextEditingController();
+
+  void _getFromGalleryCover() async {
+    XFile? pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _coverImagePath = pickedFile.path;
+        print("Cover Image Path: $_coverImagePath");
+      });
+    }
+  }
+
+  void _getFromGalleryProfile() async {
+    XFile? pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImagePath = pickedFile.path;
+        print("Cover Image Path: $_profileImagePath");
+
+      });
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -70,76 +97,9 @@ class _EditPageState extends State<EditPage> {
                           height: 110,
                           child: ClipRRect(
                             child: Image.network(
-                              url ?? user?.sampul ?? "",
-                              //_coverImagePath?.isNotEmpty ?? false ? url! : user?.sampul?? "",
+                              urlSampul ?? user?.sampul ?? "",
                               width: lebar,
                               fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                            top: 50,
-                            child: GestureDetector(
-                              onTap: () {},
-                            )),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20.0, top: 40.0),
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() async {
-                                _getFromGallery();
-                                if (user?.foto != null) {
-                                  Reference ref = FirebaseStorage.instance
-                                      .ref()
-                                      .child("users/$randomNumber.jpg");
-                                  await ref.putFile(File(user!.foto!));
-                                  url = await ref.getDownloadURL();
-                                } else if (user?.sampul != null) {
-                                  Reference ref = FirebaseStorage.instance
-                                      .ref()
-                                      .child("users/$randomNumber.jpg");
-                                  await ref.putFile(File(_coverImagePath!));
-                                  url = await ref.getDownloadURL();
-                                }
-                              });
-                            },
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    border: Border.all(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primaryContainer,
-                                      width: 2,
-                                    ),
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.edit,
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onPrimary,
-                                        size: 16,
-                                      ),
-                                      Text("  Ubah Sampul",
-                                          style: TextStyle(
-                                            fontSize: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall!
-                                                .fontSize,
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onPrimary,
-                                          ))
-                                    ],
-                                  ),
-                                ),
-                              ],
                             ),
                           ),
                         ),
@@ -158,7 +118,7 @@ class _EditPageState extends State<EditPage> {
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(100),
                               child: Image.network(
-                                user?.foto ?? "",
+                                urlProfile ?? user?.foto ?? "",
                                 width: 100,
                                 height: 100,
                                 fit: BoxFit.cover,
@@ -166,74 +126,55 @@ class _EditPageState extends State<EditPage> {
                             ),
                           ),
                         ),
-                        // Positioned(
-                        //     top: 180,
-                        //     child: GestureDetector(
-                        //       onTap: () {},
-                        //     )),
-                        // Padding(
-                        //   padding: const EdgeInsets.only(top: 150.0),
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.center,
-                        //     children: [
-                        //       Container(
-                        //         padding: EdgeInsets.all(5),
-                        //         decoration: BoxDecoration(
-                        //           shape: BoxShape.circle,
-                        //           color: Theme.of(context)
-                        //               .colorScheme
-                        //               .primaryContainer,
-                        //         ),
-                        //         child: Icon(
-                        //           Icons.edit,
-                        //           color:
-                        //               Theme.of(context).colorScheme.onPrimary,
-                        //           size: 16,
-                        //         ),
-                        //       ),
-                        //     ],
-                        //   ),
-                        // ),
                       ],
                     ),
-                    // Row(
-                    //   children: [
-                    //     Padding(
-                    //       padding: const EdgeInsets.only(top: 5.0, left: 20.0),
-                    //       child: ElevatedButton(
-                    //         onPressed: () async {
-                    //           _getFromGallery();
-                    //           String id = FirebaseAuth.instance.currentUser!.uid;
-                    //             if (user?.sampul != null) {
-                    //               Reference ref = FirebaseStorage.instance
-                    //                   .ref()
-                    //                   .child("users/$id/sampul_$randomNumber.jpg");
-                    //               await ref.putFile(File(user!.sampul!));
-                    //               url = await ref.getDownloadURL();
-                    //             }
-                    //         },
-                    //         child: Row(
-                    //           children: [
-                    //             Icon(Icons.edit),
-                    //             SizedBox(width: 8),
-                    //             Text("Ubah Sampul"),
-                    //           ],
-                    //         ),
-                    //       ),
-                    //     ),
-                    //     Spacer(),
-                    //     ElevatedButton(
-                    //         onPressed: () {},
-                    //         child: Row(
-                    //           children: [
-                    //             Icon(Icons.edit),
-                    //             SizedBox(width: 8),
-                    //             Text("Ubah Profil"),
-                    //           ],
-                    //         ),
-                    //       ),
-                    //   ],
-                    // ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            _getFromGalleryCover();
+                            if (user?.sampul != null) {
+                              String id =
+                                  FirebaseAuth.instance.currentUser!.uid;
+                              Reference ref = FirebaseStorage.instance
+                                  .ref()
+                                  .child("users/sampul_$id.jpg");
+                              await ref.putFile(File(_coverImagePath!));
+                              urlSampul = await ref.getDownloadURL();
+                            }
+                          },
+                          child: Row(
+                            children: [
+                              Icon(Icons.edit),
+                              SizedBox(width: 8),
+                              Text("Ubah Sampul"),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        ElevatedButton(
+                          onPressed: () async {
+                            _getFromGalleryProfile();
+                            if (user?.foto != null) {
+                              String id =
+                                  FirebaseAuth.instance.currentUser!.uid;
+                              Reference ref = FirebaseStorage.instance
+                                  .ref()
+                                  .child("users/profile_$id.jpg");
+                              await ref.putFile(File(_profileImagePath!));
+                              urlProfile = await ref.getDownloadURL();
+                            }
+                          },
+                          child: Row(
+                            children: [
+                              Icon(Icons.edit),
+                              SizedBox(width: 8),
+                              Text("Ubah Profil"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                     SizedBox(height: 40),
                     Padding(
                       padding: const EdgeInsets.all(20.0),
@@ -304,7 +245,7 @@ class _EditPageState extends State<EditPage> {
                           ),
                           SizedBox(height: 20),
                           ElevatedButton(
-                            onPressed: () async {
+                            onPressed: () {
                               String id =
                                   FirebaseAuth.instance.currentUser!.uid;
                               users.doc(id).update({
@@ -320,16 +261,12 @@ class _EditPageState extends State<EditPage> {
                                 "noTelp": _ctrlNomor.text.isEmpty
                                     ? user?.noTelp
                                     : _ctrlNomor.text.toString(),
-                                "foto": _profileImagePath == null
-                                    ? url
+                                "foto": urlProfile != null 
+                                    ? urlProfile
                                     : user?.foto,
-                                "sampul": _coverImagePath == null
-                                    ? (url ??
-                                        user?.sampul ??
-                                        "") // Gunakan url jika sudah diambil dari Firebase Storage
-                                    : url ??
-                                        user?.sampul ??
-                                        "", // Gunakan _coverImagePath jika tidak ada url dari Firebase Storage
+                                "sampul": urlSampul != null 
+                                    ? urlSampul
+                                    : user?.sampul,
                               });
 
                               Navigator.of(context).pop();
@@ -354,22 +291,5 @@ class _EditPageState extends State<EditPage> {
         ),
       ]),
     );
-  }
-
-  void _getFromGallery() async {
-    XFile? pickedFile;
-    try {
-      pickedFile = await ImagePicker().pickImage(
-        source: ImageSource.gallery,
-      );
-    } catch (e) {
-      print('Error picking image: $e');
-    }
-
-    if (pickedFile != null) {
-      _profileImagePath = pickedFile.path;
-      _coverImagePath = pickedFile.path;
-      _focus.requestFocus();
-    }
   }
 }
